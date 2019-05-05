8e8c61f1f62cc0a1a8680dbaf5d26c0c039b07c2
Andliexr <50293889+Andliexr@users.noreply.github.com>
2019-05-05 22:18:48 +0800

    添加主体程序

---
+++ Circleapp/Circleapp/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Circleapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string r = textBox1.Text;
+            double r1 = Convert.ToDouble(r);
+            double c = r1 * 2 * 3.14;
+            double s = r1 * r1 * 3.14;
+            label1.Text = Convert.ToString(c);
+            label2.Text = Convert.ToString(s);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+
+        }
+    }
+}
